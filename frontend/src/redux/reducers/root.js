9cbe3aff80908a4c019474f7ab63e3fcf64482b6
Arba Adhy Pamungkas <73671699+arbaap@users.noreply.github.com>
2023-05-15 23:51:27 +0700

    Delete frontend directory

--- frontend/src/redux/reducers/root.js
+++
@@ -1,8 +0,0 @@
-// ** Reducers Imports
-import auth from './auth';
-import profile from './profile';
-import authorization from './authorization';
-
-const rootReducer = { auth, profile, authorization };
-
-export default rootReducer;
--- frontend/src/repositories/profile.repo.js
+++
@@ -1,6 +0,0 @@
-import client from '../helpers/client.api';
-
-const updateProfile = (token, body) => client(token).put('/profile', body, { headers: { 'Content-Type': 'multipart/form-data' } });
-const getProfile = (token) => client(token).get('/profile');
-
-export default { getProfile, updateProfile };
